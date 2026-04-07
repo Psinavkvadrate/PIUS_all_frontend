8972639d1a307ab07876895d28efb98750da14fe
Psinavkvadrate <petrnovikovskse@gmail.com>
2026-04-07 12:23:04 +0300

    Update front

--- seller-frontend/src/shared/api/baseApi.ts
+++ seller-frontend/src/shared/api/baseApi.ts
@@ -8,7 +8,7 @@ export const baseApi = createApi({
 
     prepareHeaders: (headers) => {
       const token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTkxMTMyMC0wYzQ0LTQ0MjEtYWY5MS0wYTRkOTJiZGQ0NzMiLCJpc1NlbGxlciI6dHJ1ZX0.azhgjOSX9zVMseSagbKti7Ci2BvWW2xK_0FWa_iUt3g";
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI5OWI3MWFiNi1iMTdiLTQ5NDYtOTlhZC1lMzVjZmFiZjQ1ZGYiLCJpc1NlbGxlciI6dHJ1ZX0.vpSRNgkvXloURltgIaGB62aEbcOi6_NxCPHBkSDqyoc";
         //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI5YmE0ZWUwMS0xODZlLTQ4YTgtYTYzOC1hNjgwNGQ0ZGVmODQiLCJpc1NlbGxlciI6dHJ1ZX0.aZRdraajXo4TDSsX_wQlu7XMeBGA081R7RfjZUzDf8U";
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
