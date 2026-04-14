82618edd1cd08f6804e03867c5b5e66577c8f903
Psinavkvadrate <157591676+Psinavkvadrate@users.noreply.github.com>
2026-04-14 19:55:25 +0300

    Update vite.config.ts

--- general-login-front/vite.config.ts
+++ general-login-front/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5170,
+  },
   test: {
     environment: "jsdom",
     globals: true,
